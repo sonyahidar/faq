9c156dce9272e85c312024f3bc86e993d43b89f0
sonya <svy2@njit.edu>
2018-04-30 23:50:03 -0400

    Added: heroku database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,8 @@
 <?php
-$heroku_db_url=parse_url(env('DATABASE_URL',"postgres://forge:forge@localhost:5432/forge"));
+
+$heroku_db_url = parse_url(env('DATABASE_URL', "postgres://forge:forge@localhost:5432/forge"));
+
+
 return [
 
     /*
@@ -77,6 +80,18 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
         ],
+        'pg-heroku' => [
+            'driver' => 'pgsql',
+            'host' => $heroku_db_url['host'],
+            'database' => substr($heroku_db_url['path'], 1),
+            'username' => $heroku_db_url['user'],
+            'password' => $heroku_db_url['pass'],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+        ],
+
+
 
     ],
 
